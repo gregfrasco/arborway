500654ef83c7a70c14535516aff564cc12829c51
Greg Frasco <frascog17@gmail.com>
2020-12-21 12:36:16 -0500

    feat: about us page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
 
-export default () => (
-  <div>
-    <p>React Static is a progressive static site generator for React.</p>
-  </div>
-);
+import {Container} from "@material-ui/core";
+
+import AboutUs from '../content/about-us.mdx';
+import BoardOfDirectors from '../content/board-of-directors.mdx';
+
+export default () =>
+  <Container>
+    <AboutUs />
+    <BoardOfDirectors />
+  </Container>
+
