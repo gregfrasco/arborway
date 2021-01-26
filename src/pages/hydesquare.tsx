a598f993dfdf17917202b339eb659c0a4b3ef15e
Greg Frasco <frascog17@gmail.com>
2021-01-25 22:42:31 -0500

    feat(WIP): hyde square page

--- src/pages/hydesquare.tsx
+++ src/pages/hydesquare.tsx
@@ -1,5 +1,29 @@
 import React from 'react';
+import { Card, CardMedia, Container, Grid, Typography } from '@material-ui/core';
+import { MainFeatureCard } from 'components/main-feature-card/main-feature-card';
+
+const greenline = require('../assets/greenline.jpg');
+const welcome = require('../assets/jp-welcome.jpg');
+
+import HydeSquare from '../content/hydesquare.mdx';
 
 export default () => {
-  return <></>;
+  return (
+    <Container>
+      <MainFeatureCard image={greenline} imageText='Hero Image' />
+      <Grid container>
+        <Grid container item justify='center' xs={12}>
+          <Typography variant='h4'>The Green Line Extension from Heath Street to Hyde Square Jamaica Plain</Typography>
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <HydeSquare />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <Card>
+            <CardMedia component='img' image={welcome} height='auto' />
+          </Card>
+        </Grid>
+      </Grid>
+    </Container>
+  );
 };
