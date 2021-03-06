9c730a76742ef6a48b5d8755720945b3ac81a7d1
Greg Frasco <frascog17@gmail.com>
2021-03-06 13:12:16 -0500

    feat: merge about us mdx files

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -3,11 +3,9 @@ import React from 'react';
 import { Container } from '@material-ui/core';
 
 import AboutUs from '../content/about/about-us.mdx';
-import BoardOfDirectors from '../content/about/board-of-directors.mdx';
 
 export default () => (
   <Container>
     <AboutUs />
-    <BoardOfDirectors />
   </Container>
 );
