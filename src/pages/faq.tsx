516c058d98e928db26f2540084ba5607b69bffdc
Greg Frasco <frascog17@gmail.com>
2022-01-04 20:15:03 -0500

    feat: update FAQ

--- src/pages/faq.tsx
+++ src/pages/faq.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
-import { UnderConstruction } from 'components/under-construction/under-construction';
 import { Container, Typography } from '@material-ui/core';
+
+import FAQ from '../content/faq/faq.mdx';
 
 export default () => (
   <Container>
-    <Typography color='primary' variant='h3' component='h1'>
-      FAQ
+    <Typography color='primary' variant='h4' component='h1' style={{ marginBottom: 25, fontWeight: 'bold' }}>
+      Frequently Asked Questions
     </Typography>
-    <UnderConstruction />;
+    <FAQ />
   </Container>
 );
